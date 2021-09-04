14cef212c980bdc2112956974328c7189491997e
mccannka <mccannkellyanne@gmail.com>
2021-09-04 20:32:05 +0000

    HTML tidyup  - renoval of duplication / unused tags

---
+++ assets/js/selectrates.js
@@ -0,0 +1,13 @@
+// Existing market rates - based on competitor analysis 
+// Percentage amounts e.g. 0.10 equals 0.10%
+
+
+// Visitors who have selected they want immediate access 
+const immediateAccess = ["0.10", "0.30", "0.20", "0.10"]
+
+// Visitors who have selected that they would need monthly access 
+const regularAccess = ["0.25", "0.30", "0.50", "0.60"]
+
+// Visitors who have selected they can put money away for up to 1 year
+const fixedAccess =["0.40", "0.60", "0.90", "1.00"]
+
