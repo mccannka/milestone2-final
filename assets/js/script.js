4ba424c891124e818d98f1dc6ca2146aa601abd2
mccannka <mccannkellyanne@gmail.com>
2021-09-04 04:46:36 +0000

    JS structure / notes - function requirements

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+/*
+Wait for DOM to finish loading before running the game 
+*/
+
+/*
+Align button elements & event listeners 
+*/
+
+/*
+Function - lookup user inputs versus Competitor Rates (if / else)
+Existing rate > competitors - personalised message
+Existing rate < competitors -personalised message 
+*/
+
+/*
+Updates HTML with competitor rates 
+*/
+/*
+
+*/
+/*
+
+*/
