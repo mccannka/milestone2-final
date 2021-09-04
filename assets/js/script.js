591aefbf18709553d69755848eaef28526e84f46
mccannka <mccannkellyanne@gmail.com>
2021-09-04 05:29:02 +0000

    Competitor rates updates to JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 /*
-Wait for DOM to finish loading before running the game 
+Wait for DOM to finish loading before running calculation 
 */
 
 /*
@@ -7,17 +7,33 @@ Align button elements & event listeners
 */
 
 /*
-Function - lookup user inputs versus Competitor Rates (if / else)
-Existing rate > competitors - personalised message
-Existing rate < competitors -personalised message 
+Array of competitor rates 
 */
 
-/*
-Updates HTML with competitor rates 
-*/
-/*
+            let competitorRates = [{
+                    Competitor: 'Ulster Bank',
+                    Instant: '0.10',
+                    Fixed: '1.50',
+                    Regular: '0.25'
+                },
+                {
+                    Competitor: 'Danske Bank',
+                    Instant: '0.10',
+                    Fixed: '1.00',
+                    Regular: '0.30'
+                },
+                {
+                    Competitor: 'Santander',
+                    Instant: '0.40',
+                    Fixed: '0.90',
+                    Regular: '0.50'
+                },
+                {
+                    Competitor: 'AIB',
+                    Instant: '0.01',
+                    Fixed: '0.50',
+                    Regular: '0.10'
+                }
+            ];
 
-*/
-/*
-
-*/
+            console.log(competitorRates)
