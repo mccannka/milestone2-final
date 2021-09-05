5af3de88b540dd2ada41816f1fd33138e36f4317
mccannka <mccannkellyanne@gmail.com>
2021-09-05 00:38:39 +0000

    Define get Rates function within JS file

--- assets/js/selectrates.js
+++ assets/js/selectrates.js
@@ -2,12 +2,28 @@
 // Percentage amounts e.g. 0.10 equals 0.10%
 
 
-// Visitors who have selected they want immediate access 
-const immediateAccess = ["0.10", "0.30", "0.20", "0.10"]
+// Visitors who have selected they want instant access 
+const instantAccess = {
+    "Ulster Bank": 0.10, 
+    "Santander": 0.30, 
+    "AIB": 0.20, 
+    "Danske Bank": 0.10
+};
 
 // Visitors who have selected that they would need monthly access 
-const regularAccess = ["0.25", "0.30", "0.50", "0.60"]
+const regularAccess =   {
+"Ulster Bank": 0.25, 
+"Santander": 0.30, 
+"AIB": 0.50, 
+"Danske Bank": 0.60
+};
+
 
 // Visitors who have selected they can put money away for up to 1 year
-const fixedAccess =["0.40", "0.60", "0.90", "1.00"]
+const fixedAccess = {
+    "Ulster Bank": 0.40, 
+    "Santander": 0.60, 
+    "AIB": 0.90, 
+    "Danske Bank": 1.00
+    };
 
