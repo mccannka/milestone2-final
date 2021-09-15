299bc5b24dcf991e8708f7697bada53a1a2c4f6f
mccannka <mccannkellyanne@gmail.com>
2021-09-15 06:34:06 +0000

    Updates to js file - to include pulling data from object & updating value within HTML

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,40 +1,63 @@
-// Capturing form submission
-let accountDetailed = '';
+let savingsType = null;
+let providerIdMapper = {
+    'ulsterBank': 'ub-rate',
+    'Santander': 'sant-rate',
+    'danskeBank': 'db-rate',
+    'AIB': 'aib-rate',
+}
 
+// Define the mapping from variable to HTML option
+// "<saving var name>": "<HTML Option>" 
+let savings_options = {
+    'instant': "Instant",
+    'regular': "Regular",
+    'fixed': "Fixed",
+}
 
 $(document).ready(function () {
-    // attach the event listener
+    // Capturing form submission
+    savingsType = document.getElementById("type").value;
+
+    /*
+    On submit, runs Bootstrap validations and determines which bank is suitable
+    */
     $('#calculate').on('submit', function (event) {
         // runs when form is submitted
         // stop the default action from happening (page reloading)
         event.preventDefault();
-        const form = event.target; 
+        const form = event.target;
 
-        let savingsType = form.typeSelect.value; 
+        let savingsType = form.typeSelect.value;
         determineType(savingsType);
-    }); 
-        
+    });
+
+    // TODO: Add emailjs
+    // emailjs.init("YOUR_USER_ID");
+    // https://github.com/Code-Institute-Submissions/LeagueLore
+    // https://github.com/Code-Institute-Submissions/explore-ireland/blob/master/assets/js/email.js
 });
 
 
-let savingsType = document.getElementById("type").value;
-
-
-// Determine which rates customer wants 
-
+/* 
+Determine which savings account the visitor wants  
+*/
 function determineType(savingsType) {
-
-    if (savingsType == "Instant") {
-        accountDetailed = instantAccess;
-
-    } else if (savingsType == "Regular") {
+    if (savingsType == savings_options.instant) {
+        accountDetailed = instantAccess;  // defined in separate file
+    } else if (savingsType == savings_options.regular) {
         accountDetailed = regularAccess;
-
-    } else if (savingsType == "Fixed") {
+    } else if (savingsType == savings_options.fixed) {
         accountDetailed = fixedAccess;
     }
     console.log(accountDetailed);
+    updateHTML(accountDetailed);
 }
 
-// Get results for each competitor  
-
+/* 
+This updates the HTML data related to the banks 
+*/
+function updateHTML(data) {
+    data.forEach(function(i) {
+        $(`#${providerIdMapper[i.provider]}`).html(`${i.rate}%`)
+    });
+}
---
+++ assets/js/constants.js
@@ -0,0 +1,61 @@
+// Existing market rates - based on competitor analysis 
+// Percentage amounts e.g. 0.10 equals 0.10%
+
+
+// Visitors who have selected they want instant access 
+const instantAccess = [{
+        "provider": "ulsterBank",
+        "rate": 0.10
+    },
+    {
+        "provider": "Santander",
+        "rate": 0.30
+    },
+    {
+        "provider": "danskeBank",
+        "rate": 0.10
+    },
+    {
+        "provider": "AIB",
+        "rate": 0.20
+    },
+];
+
+// Visitors who have selected that they would need monthly access 
+const regularAccess = [{
+        "provider": "ulsterBank",
+        "rate": 0.25
+    },
+    {
+        "provider": "Santander",
+        "rate": 0.30
+    },
+    {
+        "provider": "danskeBank",
+        "rate": 0.60
+    },
+    {
+        "provider": "AIB",
+        "rate": 0.50
+    },
+];
+
+
+// Visitors who have selected they can put money away for up to 1 year
+const fixedAccess = [{
+        "provider": "ulsterBank",
+        "rate": 0.40
+    },
+    {
+        "provider": "Santander",
+        "rate": 0.60
+    },
+    {
+        "provider": "danskeBank",
+        "rate": 1.00
+    },
+    {
+        "provider": "AIB",
+        "rate": 0.90
+    },
+];
